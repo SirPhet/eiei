5e730b2ffcd90108ef69f8850189d7ec80bce2d2
Phet <phonlapat147@gmail.com>
2025-03-21 12:42:59 +0700

    initial commit

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame window = new JFrame("TowerDefense");
+
+        JFrame window = new JFrame("TowerDefenseEiei");
         window.setSize(800,600);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
--- test_git.java
+++ test_git.java
@@ -1,5 +1,5 @@
 public class test_git {
     public static void main(String[] args) {
-
+        int x = 10 ;
     }
 }
